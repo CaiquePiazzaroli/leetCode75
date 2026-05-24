a6531764f2dcef351af29767dff871e639891d4f
caique <caique405@gmail.com>
2026-05-24 12:41:43 -0300

    HashMapSet: Problem 2215

--- PC05HashMapSet/Exercise01Problem2215.java
+++ PC05HashMapSet/Exercise01Problem2215.java
@@ -6,10 +6,35 @@ import java.util.List;
 import java.util.Set;
 
 public class Exercise01Problem2215 {
-    public static void main(String[] args) {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<Integer>());
+        ans.add(new ArrayList<Integer>());
 
-        Set<Integer> nonDinstictNumbers = new HashSet<>();
-        nonDinstictNumbers.add(10);
+        for(int i : nums1) {
+            s1.add(i);
+        }
+
+        for(int i : nums2) {
+            s2.add(i);
+        }
+
+        for(int element : s1) {
+            if(!s2.contains(element)) {
+                ans.get(0).add(element);
+            }
+        }
+
+        for(int element : s2) {
+            if(!s1.contains(element)) {
+                ans.get(1).add(element);
+            }
+        }
+
+        return ans;
 
     }
 }
