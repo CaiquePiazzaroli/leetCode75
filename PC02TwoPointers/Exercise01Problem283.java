54ec895a48f5db973f111c9fe19305088e097e9f
caique <caique405@gmail.com>
2026-05-25 12:38:38 -0300

    TwoPointers: Problem 283

---
+++ PC02TwoPointers/Exercise01Problem283.java
@@ -0,0 +1,11 @@
+package PC02TwoPointers;
+
+public class Exercise01Problem283 {
+    public void moveZeroes(int[] nums) {
+        for(int element: nums) {
+            if(element == 0) {
+                
+            }
+        }
+    }
+}
